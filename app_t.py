5c551b69d3b87d40bb8d0c2db9540e22fb0c5fc4
ThaisBez01 <thaisbezerra7@yahoo.com.br>
2024-09-23 21:27:21 -0300

    Versão 1.0

---
+++ app_t.py
@@ -0,0 +1,24 @@
+def coletar_informacoes():
+    # Coletar informações do usuário
+    nome = input("Insira seu nome: ")
+    cpf = input("Insira seu CPF: ")
+    email = input("Insira seu e-mail: ")
+    endereco = input("Insira seu endereço: ")
+    genero = input("Insira seu gênero: ")
+    escolaridade = input("Insira sua escolaridade: ")
+    signo = input("Insira seu signo: ")
+    tipo_sanguineo = input("Insira seu tipo sanguíneo: ")
+
+    # Exibir as informações coletadas
+    print("\nInformações Coletadas:")
+    print(f"Nome: {nome}")
+    print(f"CPF: {cpf}")
+    print(f"E-mail: {email}")
+    print(f"Endereço: {endereco}")
+    print(f"Gênero: {genero}")
+    print(f"Escolaridade: {escolaridade}")
+    print(f"Signo: {signo}")
+    print(f"Tipo Sanguíneo: {tipo_sanguineo}")
+
+if __name__ == "__main__":
+    coletar_informacoes()
